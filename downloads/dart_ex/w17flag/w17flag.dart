603026f159eecc12764fb675d9610c832113f355
40823132 <40823132@github.com>
2020-01-01 23:47:52 +0800

    國旗改正

--- downloads/dart_ex/w17flag/w17flag.dart
+++ downloads/dart_ex/w17flag/w17flag.dart
@@ -24,58 +24,46 @@ void main() {
 }
 
 void drawROC(ctx) {
-  // 先畫滿地紅
   ctx.clearRect(0, 0, flag_w, flag_h);
   ctx.fillStyle = 'rgb(255, 0, 0)';
   ctx.fillRect(0, 0, flag_w, flag_h);
-  // 再畫青天
   ctx.fillStyle = 'rgb(0, 0, 150)';
   ctx.fillRect(0, 0, flag_w / 2, flag_h / 2);
-  // 畫十二道光芒白日
-  ctx.beginPath();
+    ctx.beginPath();
   num star_radius = flag_w / 8;
   num angle = 0;
   for (int i = 0; i < 25; i++) {
     angle += 5 * Math.pi * 2 / 12;
     num toX = 75 + Math.cos(angle) * star_radius;
     num toY = 50 + Math.sin(angle) * star_radius;
-    // 只有 i 為 0 時移動到 toX, toY, 其餘都進行 lineTo
     if (i != 0)
       ctx.lineTo(toX, toY);
     else
       ctx.moveTo(toX, toY);
   }
   ctx.closePath();
-  // 將填色設為白色
   ctx.fillStyle = '#fff';
   ctx.fill();
-  // 白日:藍圈
   ctx.beginPath();
   ctx.arc(75, 50, flag_w * 17 / 240, 0, Math.pi * 2, true);
   ctx.closePath();
-  // 填色設為藍色
   ctx.fillStyle = 'rgb(0, 0, 149)';
   ctx.fill();
-  // 白日:白心
   ctx.beginPath();
   ctx.arc(75, 50, flag_w / 16, 0, Math.pi * 2, true);
   ctx.closePath();
-  // 填色設為白色
   ctx.fillStyle = '#fff';
   ctx.fill();
 }
 
 void drawJP(ctx) {
-  // 先畫滿白
   ctx.clearRect(0, 0, flag_w, flag_h);
   ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, flag_w, flag_h);
   ctx.closePath();
-  // 紅日:紅心
   ctx.beginPath();
   ctx.arc(150, 100, flag_w / 5, 0, Math.pi * 2, true);
   ctx.closePath();
-  // 填色設為白色
   ctx.fillStyle = 'rgb(189, 0, 0)';
   ctx.fill();
 }
